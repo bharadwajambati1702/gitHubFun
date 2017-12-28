16af9abf652f989d831fcfbeffa9c7ea4cdc3dd6
Bharadwaj Ambati <bharadwajambati@Bharadwajs-MacBook-Pro.local>
2017-12-28 17:33:11 -0600

    third

--- gitHubFun/ViewController.swift
+++ gitHubFun/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor=UIColor.brown
+        self.view.backgroundColor=UIColor.blue
         // Do any additional setup after loading the view, typically from a nib.
     }
 
